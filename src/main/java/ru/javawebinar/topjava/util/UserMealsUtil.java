5aff319e40dc557cab1d9cfdee006516cb49bcc6
Evgeniy Losev <phenix.e@yandex.ru>
2019-01-28 10:11:38 -0500

    First comit

---
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -0,0 +1,31 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.model.UserMealWithExceed;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2019, Month.JANUARY, 30,20,0), "Ужин", 500)
+        );
+
+        getFilteredMealsWithExceed(mealList);
+        //toLocalDate();
+        //toLocalTime();
+    }
+
+    public static List<UserMealWithExceed> getFilteredMealsWithExceed(List<UserMeal> mealList){
+        return null;
+    }
+
+}
