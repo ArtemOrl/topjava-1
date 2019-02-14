4d2b7425022fbb3f98721569b8487e2d581fd422
Evgeniy Losev <phenix.e@yandex.ru>
2019-02-14 09:31:22 -0500

    HW1 14.02.2019 meal realisation #2 with editing and more...

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,20 +4,47 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import java.time.LocalTime;
+
 public class Meal {
-    protected final LocalDateTime dateTime;
-    protected final String description;
-    protected final int calories;
+    private Integer id;
+
+    private final LocalDateTime dateTime;
+
+    private final String description;
+
+    private final int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(null, dateTime, description, calories);
+    }
+
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
+
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
+    }
+
 
     public String getDescription() {
         return description;
@@ -27,11 +54,20 @@ public class Meal {
         return calories;
     }
 
-    public LocalDate getDate() {
-        return dateTime.toLocalDate();
+
+    public boolean isNew() {
+        return id == null;
     }
 
-    public LocalTime getTime() {
-        return dateTime.toLocalTime();
+    @Override
+    public String toString() {
+        return "Meal{" +
+                "id=" + id +
+                ", dateTime=" + dateTime +
+                ", description='" + description + '\'' +
+                ", calories=" + calories +
+                '}';
     }
+
+
 }
--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -3,16 +3,42 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealTo {
+    private Integer id;
     protected final LocalDateTime dateTime;
     protected final String description;
     protected final int calories;
-    protected final boolean exceed;
+    protected final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this(null, dateTime, description, calories, excess);
+    }
+
+    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.id = id;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.exceed = exceed;
+        this.excess = excess;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     @Override
@@ -21,7 +47,7 @@ public class MealTo {
                 "dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
-                ", exceed=" + exceed +
+                ", exceed=" + excess +
                 '}';
     }
 }
