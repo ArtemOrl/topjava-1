cb6d2c5dd583f6b222fd7ddbbaf58964bab86d76
Evgeniy Losev <phenix.e@yandex.ru>
2019-03-02 06:33:49 -0500

    02.03.2019 is working with LAZY User handling in DB

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -35,7 +35,8 @@ public class MealTestData {
 
     public static void assertMatch(Meal actual, Meal expected) {
 //        assertThat(actual).isEqualToComparingFieldByField(expected);
-        assertThat(actual).isEqualToIgnoringNullFields(expected);
+        //assertThat(actual).isEqualToIgnoringNullFields(expected);
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "user");
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
