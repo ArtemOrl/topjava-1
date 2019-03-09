f0730da796012fecef4420a641cde7496f70f740
Evgeniy Losev <phenix.e@yandex.ru>
2019-03-09 05:28:27 -0500

    09.03.2019 DataJPA realisation of MealRepository still doesn't working

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -53,3 +53,40 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
 
 
 }
+/*
+     @Autowired
+    private CrudMealRepository crudMealRepository;
+
+    @Autowired
+    private CrudUserRepository crudUserRepository;
+
+    @Override
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudMealRepository.save(meal);
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return crudMealRepository.delete(id, userId) != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        return crudMealRepository.findById(id).filter(meal -> meal.getUser().getId() == userId).orElse(null);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return crudMealRepository.getAll(userId);
+    }
+
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return crudMealRepository.getBetween(startDate, endDate, userId);
+    }
+ */
