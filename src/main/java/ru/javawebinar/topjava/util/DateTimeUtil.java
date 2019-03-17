2f0007e5d308db0970dc80265d236a0af1a73fa1
Evgeniy Losev <phenix.e@yandex.ru>
2019-03-17 14:31:15 +0300

    17.03.2019 HW6 ready with fixed tests and DateTime logic and the servlet is changed to dispatcher of Spring MVC and all tests are fixed.

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -12,8 +12,8 @@ public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     // DataBase doesn't support LocalDate.MIN/MAX
-    private static final LocalDate MIN_DATE = LocalDate.of(1, 1, 1);
-    private static final LocalDate MAX_DATE = LocalDate.of(3000, 1, 1);
+    public static final LocalDate MIN_DATE = LocalDate.of(1, 1, 1);
+    public static final LocalDate MAX_DATE = LocalDate.of(3000, 1, 1);
 
     private DateTimeUtil() {
     }
@@ -40,5 +40,24 @@ public class DateTimeUtil {
 
     public static LocalTime parseLocalTime(@Nullable String str) {
         return StringUtils.isEmpty(str) ? null : LocalTime.parse(str);
+    }
+    public static LocalDate orElse(LocalDate localDate, LocalDate localDate1){
+        // Если у нас в фильтре задано время но не задана дата, то:
+        if (localDate1.equals(MIN_DATE) && localDate==null) return MIN_DATE;
+        else if (localDate1.equals(MAX_DATE) && localDate==null) return MAX_DATE;
+        if (localDate1.equals(MIN_DATE))
+            return  localDate.isAfter(localDate) ? localDate : localDate1;
+        else // (localDate1.equals(MAX_DATE))
+            return  (localDate1.isAfter(localDate) ? localDate : localDate1);
+    }
+    public static LocalTime orElse(LocalTime localTime, LocalTime localTime1){
+        if (localTime1==LocalTime.MIN && localTime==null) return LocalTime.MIN;
+        else if (localTime1==LocalTime.MAX && localTime==null) return LocalTime.MAX;
+        if (localTime1.equals(LocalTime.MIN))
+            return localTime.isBefore(localTime1) ? localTime : localTime1;
+        else if (localTime1.equals(LocalTime.MAX))
+            return  (localTime1.isAfter(localTime) ? localTime : localTime1);
+
+        else return LocalTime.MIN;
     }
 }
